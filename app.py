d2aff1ba7c35698c5df5926590ec58aecbb68f50
AxelPribadi <64358814+AxelPribadi@users.noreply.github.com>
2024-09-05 11:06:26 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,34 +1,20 @@
-
 import streamlit as st
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 
 from model import predict_text
-
-
-# model_path = "./local_model/"
-
-# model = AutoModelForSequenceClassification.from_pretrained(model_path)
-# tokenizer = AutoTokenizer.from_pretrained(model_path)
-
-# classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
-
-
-# st.set_page_config(page_title="XLP AI Detector", page_icon=":rocket:", layout="wide", initial_sidebar_state="expanded")
 
 st.set_page_config(page_title="XLP AI Detector", page_icon=":rocket:")
 st.markdown("<h1 style='text-align: center;'>AI Content Detector</h1>", unsafe_allow_html=True)
 
-
-
 user_input = st.text_area("Enter Text", placeholder="Enter your text here...")
 
-
 if st.button("Classify"):
-    # result = classifier(user_input, truncation=True, max_length=512)
     pred, score = predict_text(user_input)
 
+    pred = "Human Text" if pred == 0 else "AI Text" 
+    score = round(score *100, 2) 
+
     st.write("Result:", pred)
-    st.write("Probabilty:", score)
+    st.write("Probabilty:", str(score), "%")
 
 
 
