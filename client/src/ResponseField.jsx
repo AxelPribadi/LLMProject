4a767db7190b63f20b9768cfbf53827a65c698d8
AxelPribadi <64358814+AxelPribadi@users.noreply.github.com>
2024-09-06 14:59:05 +0800

    Add src files

---
+++ client/src/ResponseField.jsx
@@ -0,0 +1,11 @@
+
+// Without API, this is a temporary fix to show a prototype of the app
+
+function ResponseField({response}) {
+  return (
+    <p> {response} </p>
+  )
+
+}
+
+export default ResponseField;
